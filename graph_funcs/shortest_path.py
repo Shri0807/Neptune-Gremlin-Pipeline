6e732670507c80415e8136c4d68dce6bb2a3b3e6
Shrikumar Shankar <shrikumar.shankar4@gmail.com>
2024-01-26 18:27:00 +0530

    Added Comments to graph_funcs/shortest_path.py

--- graph_funcs/shortest_path.py
+++ graph_funcs/shortest_path.py
@@ -3,6 +3,22 @@ from connect_to_neptune import connect_to_neptune
 import pandas as pd
 
 def shortest_path(**kwargs):
+    """
+    Description:
+        A function to calculate the shortest paths to a specified destination label ('Bankruptcy')
+        for nodes in a Neptune Database using the Gremlin query language.
+    
+    Parameters:
+        server: Server Name of Neptune Database
+        port: Port Number of Neptune Database (Default=8182)
+        gremlin_endpoint: Endpoint for Gremlin queries on the Neptune Database
+        nodes_local_path: Path to the nodes data directory
+        preprocessed_nodes_file_name: Name of the preprocessed nodes CSV file
+    
+    Returns:
+        df_nodes_list: DataFrame containing node information with an added
+                      'Shortest_Path' column indicating the calculated shortest paths.
+    """
 
     gremlin_endpoint = f"https://{kwargs['server']}:{int(kwargs['port'])}/{kwargs['gremlin_endpoint']}"
     nodes_path = kwargs["nodes_local_path"] + kwargs["preprocessed_nodes_file_name"]
