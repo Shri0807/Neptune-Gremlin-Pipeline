54cff3bd6504de3abb00e6400b434451fe81b639
Shrikumar Shankar <shrikumar.shankar4@gmail.com>
2024-01-26 18:39:57 +0530

    Added Comments to helper_funcs/read_config.py

--- helper_funcs/read_config.py
+++ helper_funcs/read_config.py
@@ -1,6 +1,16 @@
 import yaml
 
 def read_yaml(file):
+    """
+    Description:
+        A function to load YAML File and return a Dictionary
+    
+    Parameters:
+        file: Path of YAML File
+    
+    Returns:
+        config: Dictionary of Values in YAML File
+    """
     stream = open(file, 'r')
     config = yaml.safe_load(stream)
 
